3c9be14f5b56fa474c0f670edbda3213dd8b9ea1
Jim Urbas <git@jimu.net>
2021-09-02 01:51:21 -0500

    Add book table and routs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,25 @@
 class BooksController < ApplicationController
   def show
+    render json: Book.all
+  end
+
+  def create
+    book = Book.new(book_params) # title: params[:title], author: params[:author]
+    if book.save
+      render json: book, status: :created      # 201
+    else
+      render json: book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    Book.delete @id
+  end
+
+  private
+
+  def book_params
+    params.require(:book)
+    params.permit :title, :author
   end
 end
